411273c4033353b771c49123ab665a805d33a19b
Sidonie <sidonie.jung@quickms.fr>
2021-11-15 11:29:20 +0100

    refonte html/css

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -9,7 +9,7 @@ function editNav() {
 
 // DOM Elements
 const modalbg = document.querySelector(".bground");
-const modalBtn = document.querySelectorAll(".modal-btn");
+const modalBtn = document.querySelectorAll(".btn-signup");
 const formData = document.querySelectorAll(".formData");
 
 // launch modal event
