09655b76032c4eb9c50c9f780f618ff1923db208
Sidonie <sidonie.jung@quickms.fr>
2021-11-22 14:59:07 +0100

    JS validation nom et mail

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -4,7 +4,7 @@
 
 function editNav() {
   var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
+  if (x.className == "topnav") {
     x.className += " responsive";
   } else {
     x.className = "topnav";
@@ -37,9 +37,11 @@ function closeModal() {
 /* Unvalidation input */
 /**********************/
 
+// TODO : voir comment ne pas garder en mémoire les réponses apportées précédement dans le form
+
 const formData = document.querySelectorAll(".formData");
 const checkboxInputsTown = document.querySelectorAll(".checkbox-input-town");
-
+const inputsNameParents = document.querySelectorAll('.validationName');
 
 // status changed when you click on a radio box to chose your town
 //toggle() change état d'un élément à chaque fois qu'elle est appelée
@@ -66,7 +68,7 @@ function checkboxValidate() {
     // fonction flechée qui retourne un bool
     let isChecked =  array.some((checkbox)=> checkbox.checked);
     const parent = array[0].parentNode;
-    if(isChecked===false) {
+    if(isChecked==false) {
         parent.setAttribute("data-error-visible", "true");
     }else{
         parent.setAttribute("data-error-visible", "false");
@@ -74,24 +76,68 @@ function checkboxValidate() {
     }
 }
 
-// // form submit validation 
-function validate(event) {
-    event.preventDefault();
-    if (checkboxValidate() === true) {
-        return replaceTextValidation();
+
+
+// firstname et lastname validation
+function nameValidate() {
+
+    const inputfirst = document.getElementById('first').value.length;
+    const inputlast = document.getElementById('last').value.length;
+    const parent = document.querySelectorAll(".validationName");
+
+            if (inputfirst < 2) {
+                parent[0].setAttribute("data-error-visible", "true");
+            } else if (inputlast < 2){
+                parent[1].setAttribute("data-error-visible", "true");
+            } else {
+                parent[0].setAttribute("data-error-visible", "false");
+                parent[1].setAttribute("data-error-visible", "false");
+                return true;
+            }
+}
+
+
+// email validation
+
+function mailValidate() {
+    const email = document.getElementById("email");
+    const parent = document.querySelectorAll(".validationMail");
+
+
+    if(!email.validity.valid) {
+        parent[0].setAttribute("data-error-visible", "true");
+        if(email.validity.typeMismatch == true && email.validity.patternMismatch == true ) {
+            debugger
+            parent[0].setAttribute("data-error", "Veuillez entrer une adresse email valide.");
+        } else if(email.validity.valueMissing == true) {
+            parent[0].setAttribute("data-error", "Veuillez entrer une adresse email.");
+        } else {
+            parent[0].setAttribute("data-error-visible", "false");
+        }
+    } else {
+        parent[0].setAttribute("data-error-visible", "false");
+        return true;
     }
 
-//   switch (document.getAttribute("id")) {
-//     case 'first' : 
-//       if (input.value.length, 2 === false) {
-//         // changer la valeur de data-error-visible en true
-//         // empecher la validation du formulaire
-//       }
-//     break;
-// //     case 'last' : 
-// //     break
-// //   }
+}
 
+// validation date de naissance
+
+function birthdayValidation() {
+    const dateControl = document.querySelector('input[type="date"]');
+
+    dateControl.minValue = '2017-06-01';
+
+}
+
+
+
+// form submit validation 
+function validate(event) {
+    event.preventDefault();
+    if (checkboxValidate()==true && nameValidate()==true &&  mailValidate()==true) {
+        return replaceTextValidation();
+    }
 }
 
 
